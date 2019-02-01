d4566b5c5fb616f28a20589172b10c67634e8b8c
Kostya Kortchinsky <kostyak@google.com>
2019-02-01 09:06:42 -0800

    extra: product: Add option to disable Scudo globally
    This adds an option to turn off Scudo globally, and use it for Go.
    
    Bug: 123228023
    Test: verify that Scudo is disabled for a Go build, eg:
    lunch marlin_svelte-eng && m -j, check that Scudo is not linked in
    out/target/product/marlin/system/bin/mediaextractor
    Test: verify that Scudo is enabled otherwise, eg:
    lunch marlin-eng && m -j, check that Scudo is linked in
    out/target/product/marlin/system/bin/mediaextractor
    
    Change-Id: Idc82d581fade544a474e6f2ff0b54dd191ba0818
    Merged-In: Idc82d581fade544a474e6f2ff0b54dd191ba0818

--- product/go.mk
+++ product/go.mk
@@ -15,3 +15,8 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 # generic profile and not Android Go optimized.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Disable Scudo outside of eng builds to save RAM.
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
+  PRODUCT_DISABLE_SCUDO := true
+endif
