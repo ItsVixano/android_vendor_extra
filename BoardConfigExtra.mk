bf8e04ef5311a5ed270fc4d52385fe04be097fe5
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    extra: props: Enable usage of dex2oat64
    Use 64-bit dex2oat for better dexopt time.
    
    Bug: 153380900
    Test: boot and install an application
    Change-Id: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095
    (cherry picked from commit 126f03be80f57a8a0411842011152d9381589b78)
    Merged-In: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -8,6 +8,7 @@ VENDOR_EXTRA_PATH := vendor/extra
 
 # Props
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(VENDOR_EXTRA_PATH)/props/system_ext.prop
 ifneq ($(filter msm8953 hi6250,$(TARGET_BOARD_PLATFORM)),)
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/go_vendor.prop
 TARGET_SYSTEM_EXT_PROP += $(VENDOR_EXTRA_PATH)/props/go_system_ext.prop
