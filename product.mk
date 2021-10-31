429ddb6e7395100b6b513b101f8fef7dce54ff3b
Nolen Johnson <johnsonnolen@gmail.com>
2021-10-31 11:49:04 -0400

    extra: Conditionally build ih8sn for a few devices
    Change-Id: I3c83bbc34ce59347e85b9e45ce0582bc3029e388

--- product.mk
+++ product.mk
@@ -15,3 +15,15 @@ PRODUCT_PACKAGES += \
 # Apps
 PRODUCT_PACKAGES += \
     GrapheneCamera
+
+# ih8sn
+PRODUCT_PACKAGES += \
+    ih8sn
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/etc/ih8sn.conf.$(subst lineage_,,$(TARGET_PRODUCT)):/system/etc/ih8sn.conf
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/ih8sn \
+    system/etc/ih8sn.conf \
+    system/etc/init/ih8sn.rc
