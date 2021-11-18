2dccb10a98df5fc6ce07ffa055b0fc25f6f407fd
Danny Lin <danny@kdrag0n.dev>
2021-11-17 19:42:10 -0800

    extra: Always use release-keys for ro.build.tags
    This can no longer be overridden elsewhere now that duplicate sysprops
    are detected as errors. BUILD_KEYS is preserved to keep the display
    build ID and other system info accurate.
    
    [ItsVixano: Apply this only for `dev-keys` builds]
    [ItsVixano: Use PRODUCT_BUILD_PROP_OVERRIDES instead]
    
    Change-Id: If1d3f3af4e47b1b11b0b9bc1ad55cc6207db497b

--- product.mk
+++ product.mk
@@ -42,3 +42,9 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Spoof `dev-keys` builds into `release-keys`
+ifneq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/make/target/product/security/testkey)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_VERSION_TAGS="release-keys"
+endif
