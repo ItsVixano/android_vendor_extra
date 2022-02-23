09dd02e1bdd2ec6b522f7a0bd335da967ac10bf7
Jarl-Penguin <jarlpenguin@outlook.com>
2022-02-23 15:31:08 +0100

    extra: soong: Add TARGET_QCOM_NO_FM_FIRMWARE conditional
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I83cbf855839a57c3506f0d8b6c538224a2a148e3

--- config/BoardConfigExtraSoong.mk
+++ config/BoardConfigExtraSoong.mk
@@ -3,3 +3,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Setup SOONG_CONFIG_* vars to export the vars listed above.
+# Documentation here:
+# https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
+
+SOONG_CONFIG_lineageQcomVars += \
+    qcom_no_fm_firmware \
+
+# Soong value variables
+SOONG_CONFIG_lineageQcomVars_qcom_no_fm_firmware := $(TARGET_QCOM_NO_FM_FIRMWARE)
