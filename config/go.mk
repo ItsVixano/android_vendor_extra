356555dd68b1a1b7f881ec7a8464d472dc242c54
Jared Duke <jdduke@google.com>
2022-02-28 09:35:16 -0800

    extra: config: Reland: Enable R8 code shrinking for system_server on Pixel
    This change landed in ag/16941691 but was rolled back due to
    issues with stripping of CustomScrollView and PackageManager
    shell command support. That issue has since been fixed.
    
    Original change description:
    Opt in to whole-program Java optimizations via R8 for Pixel devices.
    This can be explicitly disabled during builds via
    `SYSTEM_OPTIMIZE_JAVA=false m`.
    
    Note that this change only enables code shrinking for system_server,
    and should not impact stack traces. Additional R8 optimizations
    will be enabled after broader improvements to retracing infrastructure.
    See go/aperf-system-r8-faq for additional details and guidance. Also
    note that R8 shrinking optimizations are implicitly disabled in ENG builds.
    
    Enabling for non-Pixel devices will proceed in stages as we flesh
    out infrastructure and validate with test populations.
    
    Results: services.jar (20MB -> 17MB)
    
    Bug: 210510433
    Test: SYSTEM_OPTIMIZE_JAVA=true + atest
      CtsInstalledLoadingProgressHostTests
      CtsAutoFillServiceTestCases
      CtsAppSecurityHostTestCases
      CtsAppTestCases
    Change-Id: I7f64819a8d23a39e7afbb1a210c229f223855243

--- config/go.mk
+++ config/go.mk
@@ -16,8 +16,9 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
-# Enable whole-program R8 Java optimizations for SystemUI, but also
-# allow explicit overriding for testing and development.
+# Enable whole-program R8 Java optimizations for SystemUI and system_server,
+# but also allow explicit overriding for testing and development.
+SYSTEM_OPTIMIZE_JAVA ?= true
 SYSTEMUI_OPTIMIZE_JAVA ?= true
 
 # Disable Scudo outside of eng builds to save RAM.
