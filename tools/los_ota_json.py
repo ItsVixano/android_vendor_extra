e3347eb7a5ac48095d7e4820d04261ddabb3ed91
Giovanni Ricca <giovanniricca@proton.me>
2022-07-10 17:49:44 +0200

    extra: tools: Add a script for generating LineageOS OTA jsons
    Change-Id: If5bf4b86c083164da9ee1b2b27b82d6b94828d13

---
+++ tools/los_ota_json.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+#
+# Copyright (C) 2022 Giovanni Ricca
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import os
+from hashlib import md5
+from re import search
+from glob import glob
+
+
+def getprop(prop):
+    return search(
+        r"".join(["(?<=", prop, "=).*"]), open("system/build.prop").read()
+    ).group(0)
+
+
+rom_name = "lineage"
+version, datetime, incremental = (
+    getprop("ro.lineage.build.version"),  # version
+    getprop("ro.build.date.utc"),  # datetime
+    getprop("ro.build.version.incremental"),  # incremental
+)
+filename = max(
+    glob("".join([rom_name, "-", version, "*", ".zip"])),
+    key=os.path.getctime,
+)
+id = md5(open(filename, "rb").read()).hexdigest()
+size = os.stat(filename).st_size
+
+print(
+    """
+{
+  "response": [
+    {
+      "datetime": %s,
+      "filename": "%s",
+      "id": "%s",
+      "romtype": "unofficial",
+      "size": %s,
+      "url": " ",
+      "version": "%s"
+    }
+  ]
+}
+
+Place a dummy json file named "%s.json"
+"""
+    % (datetime, filename, id, size, version, incremental)
+)
