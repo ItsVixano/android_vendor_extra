cc895481546d4a4a5dd35be6bbf5b2bf761acd40
Giovanni Ricca <giovanniricca@proton.me>
2022-07-21 14:40:06 +0200

    extra: build: Add tools/los_ota_json.py as a bash function
    Change-Id: I686fc88b7a1801e3d926c015023e9e10b33faf7e

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -7,7 +7,14 @@
 
 . build/envsetup.sh
 
-export VENDOR_PATCHES_PATH=$(gettop)/vendor/extra/build/patches
+export VENDOR_EXTRA_PATH=$(gettop)/vendor/extra/
+export VENDOR_PATCHES_PATH="$VENDOR_EXTRA_PATH"/build/patches
+
 . "$VENDOR_PATCHES_PATH"/apply-patches.sh
 
 unset JAVAC
+
+# functions
+los_ota_json() {
+    "$VENDOR_EXTRA_PATH"/tools/los_ota_json.py
+}
