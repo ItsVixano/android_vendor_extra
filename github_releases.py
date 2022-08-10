9ecd6ef88b54c248d64dd3e3f3ea78a577a7d844
Giovanni Ricca <giovanniricca@proton.me>
2022-08-11 00:47:07 +0200

    GitHub_release-script: Calculate sha1sum automatically
    Change-Id: I2f589083446916fff490d12d2ef86d1a3883e130

--- github_releases.py
+++ github_releases.py
@@ -7,6 +7,7 @@
 
 import sys
 import os
+import hashlib
 from dotenv import load_dotenv
 from github import Github
 
@@ -30,6 +31,18 @@ def get_device(var):
     }.get(var)
 
 
+def sha1sum(var):
+    file_hash = hashlib.sha1()
+    BLOCK_SIZE = 15728640  # 15mb
+    with open("uploads/" + var, "rb") as f:
+        fb = f.read(BLOCK_SIZE)
+        while len(fb) > 0:
+            file_hash.update(fb)
+            fb = f.read(BLOCK_SIZE)
+
+    return file_hash.hexdigest()
+
+
 # Vars
 load_dotenv()
 GH_TOKEN = os.getenv("TOKEN")
@@ -38,6 +51,17 @@ GH_REPO = sys.argv[2]  # Github repo name
 GH_TAG = sys.argv[3]  # Github release tag name
 GH_BODY = open("release_body.txt", "r").read()
 GH_NAME = f"LineageOS 19.1 for {get_device(GH_REPO)[1]} ({GH_TAG})"
+
+# Calculate the sha1sums of the assets
+assets = os.listdir("uploads")
+first_line = True
+for asset in assets:
+    print(f"\nCalculating sha1sum for `{asset}`")
+    if first_line == True:
+        GH_BODY += f"- {asset}: `{sha1sum(asset)}`"
+        first_line = False
+    else:
+        GH_BODY += f"\n- {asset}: `{sha1sum(asset)}`"
 
 # Create release
 git = Github(GH_TOKEN)
@@ -48,10 +72,12 @@ repo.create_git_release(
 
 # Grab the output after wait
 GH_DRAFT_TAG = input(
-    f"Go on: https://github.com/{GH_OWNER}/{GH_REPO}/releases, Select the draft release and paste the release id here (it starts with 'untagged-')\n"
+    f"\nGo on https://github.com/{GH_OWNER}/{GH_REPO}/releases, Select the draft release and paste the release id from the url (it starts with `untagged-`)\n"
 )
 
 # Upload assets
-assets = os.listdir("uploads")
 for asset in assets:
+    print(f"\nUploading `{asset}`")
     repo.get_release(GH_DRAFT_TAG).upload_asset(f"uploads/{asset}")
+
+print("\nDone!")
