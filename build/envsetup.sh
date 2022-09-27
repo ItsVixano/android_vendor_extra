e73abc18c273fa41183c750977f3109b07b01f96
Giovanni Ricca <giovanniricca@proton.me>
2022-09-27 14:56:48 +0200

    extra: build: Refractor envsetup
    Change-Id: I51f50c1c15ac9c6601c50ccc15c8ac95ed6c2db6

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -8,10 +8,9 @@
 if [[ "$1" = "-h" || "$1" = "--help" ]]; then
     cat <<'END'
 Usage:
- . vendor/extra/build/envsetup.sh [ARGS]
+ . vendor/extra/build/envsetup.sh [ARG]
      -h/--help: Shows this screen
      -p/--apply-patches: Apply the patches inside vendor/extra/build/patches folder
-     -d/--dirty: Avoids running 'mka installclean' before building
 END
     return 0
 fi
@@ -30,10 +29,40 @@ fi
 
 # functions
 los_ota_json() {
+    if [[ "$1" = "-h" || "$1" = "--help" ]]; then
+        cat <<'END'
+Usage:
+ los_ota_json [DEVICE CODENAME]
+END
+        return 0
+    fi
+
+    # Check is $1 is empty
+    if [ -z "$1" ]; then
+        echo -e "\nPlease mention the device codename first"
+        return 0
+    fi
+
+    # Defs
+    DEVICE="$1"
+
+    # Generate the OTA Json
+    croot
+    cd out/target/product/"$DEVICE"/ &> /dev/null
     "$VENDOR_EXTRA_PATH"/tools/los_ota_json.py
 }
 
 mka_build() {
+    # Call for help
+    if [[ "$1" = "-h" || "$1" = "--help" ]]; then
+        cat <<'END'
+Usage:
+ mka_build [DEVICE CODENAME] [ARG]
+     -d/--dirty: Avoids running 'mka installclean' before building
+END
+        return 0
+    fi
+
     # Goofy ahh build env
     unset JAVAC
 
@@ -43,21 +72,21 @@ mka_build() {
         return 0
     fi
 
-    # Default to clean build
-    if [[ "$2" = "-d" || "$2" = "--dirty" ]]; then
-        echo -e "\nWarning: Building without cleaning up $DEVICE out dir"
-        DIRTY_BUILD="no"
-    else
-        echo -e "\nWarning: Building with cleaned up $DEVICE out dir"
-        DIRTY_BUILD="yes"
-    fi
-
     # Defs
     DEVICE="$1"
     BUILD_TYPE="userdebug" # ToDo: Don't hardcode it
+    if [[ "$2" = "-d" || "$2" = "--dirty" ]]; then
+        echo -e "\nWarning: Building without cleaning up $DEVICE out dir\n"
+        DIRTY_BUILD="no"
+    else
+        echo -e "\nWarning: Building with cleaned up $DEVICE out dir\n"
+        DIRTY_BUILD="yes"
+    fi
+
+    sleep 3
 
     # Build
-    croot # Make sure we are running this on source rootdir
+    croot # Make sure we are inside the source root dir
     lunch lineage_"$DEVICE"-"$BUILD_TYPE"
     if [ "$DIRTY_BUILD" = "yes" ]; then
         mka installclean
@@ -65,16 +94,15 @@ mka_build() {
     mka bacon -j4
 
     # Upload build + extras
-    cd out/target/product/"$DEVICE"/
-    cp lineage-19*.zip ~/public_html/giovanni/"$DEVICE"/
-    cp recovery.img ~/public_html/giovanni/"$DEVICE"/
-    cp boot.img ~/public_html/giovanni/"$DEVICE"/
-    cp obj/PACKAGING/target_files_intermediates/*/IMAGES/vendor_*.img ~/public_html/giovanni/"$DEVICE"/
-    cp dtbo.img ~/public_html/giovanni/"$DEVICE"/
+    cd out/target/product/"$DEVICE"/ &> /dev/null
+    cp lineage-19*.zip ~/public_html/giovanni/"$DEVICE"/ &> /dev/null
+    cp recovery.img ~/public_html/giovanni/"$DEVICE"/ &> /dev/null
+    cp boot.img ~/public_html/giovanni/"$DEVICE"/ &> /dev/null
+    cp obj/PACKAGING/target_files_intermediates/*/IMAGES/vendor_*.img ~/public_html/giovanni/"$DEVICE"/ &> /dev/null
+    cp dtbo.img ~/public_html/giovanni/"$DEVICE"/ &> /dev/null
 
     # Output OTA JSON
-    los_ota_json
-    croot
+    los_ota_json "$DEVICE"
 
     echo -e "\n\nDone!"
 }
