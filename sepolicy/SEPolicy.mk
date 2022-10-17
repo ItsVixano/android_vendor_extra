608727b203fba8973e002a96c6a3eddb73bda340
Alexander Martinz <amartinz@shiftphones.com>
2022-10-17 19:02:58 +0200

    extra: sepolicy: allow apexd to write to sysfs_mmc_host
    As qualcomm relabels read_ahead_kb and friends as sysfs_mmc_host
    we explicitly need to grant apexd access to it or it will break.
    
    This results in eg GSIs to be unbootable.
    
    type=1400 audit(3799551.036:40): avc: denied { read write }
      for comm="apexd" name="read_ahead_kb" dev="sysfs" ino=81305
      scontext=u:r:apexd:s0 tcontext=u:object_r:sysfs_mmc_host:s0
      tclass=file permissive=0
    
    Change-Id: Iea24b94318893e8526e06e24bc3308acba37b0cc
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -6,8 +6,14 @@
 
 EXTRA_SEPOLICY_PATH := vendor/extra/sepolicy
 
+## Vendor ##
+
+# Common
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(EXTRA_SEPOLICY_PATH)/vendor/common
+
 # Fm radio
 ifeq ($(BOARD_HAVE_QCOM_FM), true)
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(EXTRA_SEPOLICY_PATH)/fm/vendor
+    $(EXTRA_SEPOLICY_PATH)/vendor/fm
 endif
