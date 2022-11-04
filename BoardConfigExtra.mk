105ffbcef7949140df0e5b6e7cfa8ad8b30041d9
Giovanni Ricca <giovanniricca@proton.me>
2022-11-04 12:32:55 +0100

    extra: prop: Inherit Android Go system props for msm8953
    https://github.com/LineageOS/android_build/blob/lineage-20.0/target/board/go_defaults_common.prop
    
    Change-Id: I9944e3310200c5d71147cf8cbc2155d72ae75188

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -8,6 +8,9 @@ VENDOR_EXTRA_PATH := vendor/extra
 
 # Props
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/vendor.prop
+ifeq ($(TARGET_BOARD_PLATFORM), msm8953)
+TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/go_vendor.prop
+endif
 
 # SEPolicy
 include vendor/extra/sepolicy/SEPolicy.mk
