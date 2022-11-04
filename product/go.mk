92a0b77db96e83a3f27f7508953499dc90168136
Giovanni Ricca <giovanniricca@proton.me>
2022-11-04 12:22:21 +0100

    extra: product: Enable Android Go confifurations
    * They don't have a splitted authorship so take it :D
    https://github.com/LineageOS/android_build/commit/0a0e5ea5338db8103eec54ae7bdf9b58f8f7a93f
    
    Change-Id: I6f4207d0f8f105ec814777479457ade6d7d6a0f5

--- product/go.mk
+++ product/go.mk
@@ -3,3 +3,15 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Always preopt extracted APKs to prevent extracting out of the APK for gms
+# modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
