19e3b0f650b6acf303201b60de73dde327179990
Giovanni Ricca <giovanniricca@proton.me>
2022-11-05 21:05:50 +0100

    extra: props: Move props on vendor
    * Make it treble compatible
    
    Change-Id: I548333230be1db5f827ab450649f95db2babae7b

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -7,7 +7,7 @@
 VENDOR_EXTRA_PATH := vendor/extra
 
 # Props
-TARGET_SYSTEM_PROP += $(VENDOR_EXTRA_PATH)/props/system.prop
+TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/vendor.prop
 
 # SEPolicy
 include vendor/extra/sepolicy/SEPolicy.mk
