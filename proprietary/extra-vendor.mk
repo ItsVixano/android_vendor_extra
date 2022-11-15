101d38ce75c7d1e75870a02783e17f3713f845ab
Giovanni Ricca <giovanniricca@proton.me>
2022-11-15 15:36:54 +0100

    extra: proprietary: Fixup aptx libs copy path
    * COY is the new COPY
    
    Change-Id: I80ae9dfb02bf4763fca5fc84cdcb65f36a59f579

--- proprietary/extra-vendor.mk
+++ proprietary/extra-vendor.mk
@@ -6,5 +6,5 @@
 
 # aptX
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system_ext/lib64/libaptX_encoder.so:$(TARGET_COY_OUT_SYSTEM_EXT)/lib64/libaptX_encoder.so \
-    $(LOCAL_PATH)/system_ext/lib64/libaptXHD_encoder.so:$(TARGET_COY_OUT_SYSTEM_EXT)/lib64/libaptXHD_encoder.so
+    $(LOCAL_PATH)/system_ext/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libaptX_encoder.so \
+    $(LOCAL_PATH)/system_ext/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libaptXHD_encoder.so
