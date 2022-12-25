c1317e1ab4dd94eb80f2c92c7d6599353b1edbf4
Giovanni Ricca <giovanniricca@proton.me>
2022-12-25 18:21:48 +0100

    extra: tools: Fill out the download url automatically
    Change-Id: I9296c03f9218a004caf56b8f9bfd28655e310ea7

--- tools/los_ota_json.py
+++ tools/los_ota_json.py
@@ -7,7 +7,7 @@
 
 import os
 from hashlib import md5
-from re import search
+from re import search, sub
 from glob import glob
 
 
@@ -17,10 +17,11 @@ def getprop(prop):
     ).group(0)
 
 
-version, datetime, incremental = (
+version, datetime, incremental, codename = (
     getprop("ro.lineage.build.version"),  # version
     getprop("ro.build.date.utc"),  # datetime
     getprop("ro.build.version.incremental"),  # incremental
+    getprop("ro.lineage.device"),  # codename
 )
 filename = max(
     glob("".join(["lineage-", version, "*", ".zip"])),
@@ -28,6 +29,16 @@ filename = max(
 )
 id = md5(open(filename, "rb").read()).hexdigest()
 size = os.stat(filename).st_size
+url = "".join(
+    [
+        "https://github.com/ItsVixano-releases/LineageOS_",
+        codename,
+        "/releases/download/",
+        sub("[^0-9]", "", incremental)[:-6],
+        "/",
+        filename,
+    ]
+)
 
 print(
     """
@@ -39,7 +50,7 @@ print(
       "id": "%s",
       "romtype": "unofficial",
       "size": %s,
-      "url": " ",
+      "url": "%s",
       "version": "%s"
     }
   ]
@@ -47,5 +58,5 @@ print(
 
 Place a dummy json file named "%s.json"
 """
-    % (datetime, filename, id, size, version, incremental)
+    % (datetime, filename, id, size, url, version, incremental)
 )
