cc02d3d7f13bf7bba275e4a2416a51ddcadb9168
Giovanni Ricca <giovanniricca@proton.me>
2022-12-27 12:59:26 +0100

    extra: sepolicy: Fix build on los19
    Change-Id: I47eb51359fdd0d0c97b9f4fa878c13e10dbc3abb

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -9,11 +9,19 @@ EXTRA_SEPOLICY_PATH := vendor/extra/sepolicy
 ## Vendor ##
 
 # Common
+ifeq ($(PRODUCT_VERSION_MAJOR), 20)
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(EXTRA_SEPOLICY_PATH)/vendor/common
+endif
 
 # Fm radio
 ifeq ($(BOARD_HAVE_QCOM_FM), true)
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(EXTRA_SEPOLICY_PATH)/vendor/fm
-endif
+
+ifeq ($(PRODUCT_VERSION_MAJOR), 19)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    $(EXTRA_SEPOLICY_PATH)/private/fm
+endif # ($(PRODUCT_VERSION_MAJOR), 19)
+
+endif # ($(BOARD_HAVE_QCOM_FM), true)
