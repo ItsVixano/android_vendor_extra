d1267dea9bc3882834a7515af75fa16377930f0a
Giovanni Ricca <giovanniricca@proton.me>
2023-04-04 07:45:58 +0200

    extra: Kill FM2 once and for all
    * FMRadio got resurrected!!!
    
    Change-Id: I70348a530f20a0d0c6fa23da55060d05125286a0

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -11,9 +11,3 @@ TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/vendor.prop
 ifeq ($(TARGET_BOARD_PLATFORM), msm8953)
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/go_vendor.prop
 endif
-
-# SEPolicy
-include vendor/extra/sepolicy/SEPolicy.mk
-
-# Soong vars
-include vendor/extra/config/BoardConfigExtraSoong.mk
