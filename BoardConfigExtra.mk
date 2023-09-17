1c444b508c7df18679be08d90289f6b4fd5e690f
Giovanni Ricca <giovanniricca@proton.me>
2023-09-18 00:10:07 +0200

    extra: props: Move avoid_gfx_accel prop on system_ext
    * Selinux ;-;
    
    Change-Id: I1c40828762597953a3c670903c95c0b65cd250c7

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -10,4 +10,5 @@ VENDOR_EXTRA_PATH := vendor/extra
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/vendor.prop
 ifneq ($(filter msm8953 hi6250,$(TARGET_BOARD_PLATFORM)),)
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/go_vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(VENDOR_EXTRA_PATH)/props/go_system_ext.prop
 endif
