bd67eef4737709eb3a3dbbfcdb632aa7bdc2b568
Giovanni Ricca <giovanniricca@proton.me>
2023-09-24 10:44:48 +0200

    extra: Sort product.mk
    Change-Id: I59a7b28a1e6a78fe0ff817d52c66179c7a105cdd

--- product.mk
+++ product.mk
@@ -10,16 +10,13 @@ $(call inherit-product, vendor/extra/config/go.mk)
 # Inherit ih8sn Makefile
 $(call inherit-product, vendor/extra/external/ih8sn/product.mk)
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
-
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Rootdir
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/bin/neofetch:$(TARGET_COPY_OUT_SYSTEM)/bin/neofetch
+# LDAC
+PRODUCT_PACKAGES += \
+    libldacBT_enc \
+    libldacBT_abr
 
 # Overlays
 PRODUCT_PACKAGES += \
@@ -27,7 +24,10 @@ PRODUCT_PACKAGES += \
     LineageUpdaterOverlay$(PRODUCT_VERSION_MAJOR) \
     RippleSystemUIOverlay
 
-# LDAC
-PRODUCT_PACKAGES += \
-    libldacBT_enc \
-    libldacBT_abr
+# Rootdir
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/bin/neofetch:$(TARGET_COPY_OUT_SYSTEM)/bin/neofetch
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
