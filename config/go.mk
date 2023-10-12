4fa2a20a0e57a91612284ccaff8af0aab1bdd459
Giovanni Ricca <giovanniricca@proton.me>
2023-10-12 19:24:21 +0200

    extra: config: Don't make R8 javac flags overridable
    Change-Id: Icb50faede99d8d56bc81a7514c18be60674bd9e3

--- config/go.mk
+++ config/go.mk
@@ -17,9 +17,8 @@ PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # Enable whole-program R8 Java optimizations for SystemUI and system_server,
-# but also allow explicit overriding for testing and development.
-SYSTEM_OPTIMIZE_JAVA ?= true
-SYSTEMUI_OPTIMIZE_JAVA ?= true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
 
 # Disable Scudo outside of eng builds to save RAM.
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
