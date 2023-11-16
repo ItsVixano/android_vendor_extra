4c25321a80ffbaac638b67a43a381aba993f0a5d
Giovanni Ricca <giovanniricca@proton.me>
2023-11-16 23:00:55 +0000

    extra: Build with Jemalloc
    * MiuiCamera crashes on miatoll with scudo
    
    * MALLOC_SVELTE is an AOSP var [1]
    * MALLOC_SVELTE_FOR_LIBC32 depends on [2] chain
    
    [1] https://android.googlesource.com/platform/bionic/+/HEAD/docs/native_allocator.md
    [2] https://review.lineageos.org/q/topic:%22twelve-scudo-free-libc32%22
    
    Change-Id: I2e27d134e83859b2b2011e9a068f92b1f71dc741

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -11,6 +11,13 @@ ifneq ($(filter atoll lahaina,$(TARGET_BOARD_PLATFORM)),)
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
 endif
 
+# Camera - MiuiCamera
+# To use with external/jemalloc_new from AOSPA
+ifeq ($(TARGET_BOARD_PLATFORM), atoll)
+MALLOC_SVELTE := true
+MALLOC_SVELTE_FOR_LIBC32 := true
+endif
+
 # Props
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/vendor.prop
 TARGET_SYSTEM_EXT_PROP += $(VENDOR_EXTRA_PATH)/props/system_ext.prop
