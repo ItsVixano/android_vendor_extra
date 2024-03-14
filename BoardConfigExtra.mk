00d45778adf6c7682fcce3cc9a5288a553b6256a
Giovanni Ricca <giovanniricca@proton.me>
2024-03-14 18:14:52 +0100

    Add `hi3660` as legacy device
    Change-Id: Ibc1c34c3c05ef6367b03b1a389a197eb4502bec0

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -24,7 +24,7 @@ endif
 # Props
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/vendor.prop
 TARGET_SYSTEM_EXT_PROP += $(VENDOR_EXTRA_PATH)/props/system_ext.prop
-ifneq ($(filter msm8953 hi6250,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8953 hi3660 hi6250,$(TARGET_BOARD_PLATFORM)),)
 TARGET_VENDOR_PROP += $(VENDOR_EXTRA_PATH)/props/go_vendor.prop
 TARGET_SYSTEM_EXT_PROP += $(VENDOR_EXTRA_PATH)/props/go_system_ext.prop
 endif
